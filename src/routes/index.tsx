b7a21eb2ca1383e88283862a4b3ab6dbbeac6837
ice <icebiter@outlook.com>
2021-12-26 18:05:16 +0800

    修复登陆不能跳转的问题

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,11 @@ import {
     ToastComponent,
     AlertComponent,
 } from 'amis';
-import { Route, Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
 import { observer } from 'mobx-react';
 import { IMainStore } from '../stores';
 import Login from './Login';
-import Register from './Register';
 import AdminRoute from './admin';
-import NotFound from './404';
 
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
@@ -19,23 +17,15 @@ export default observer(function({store}:{
     store:IMainStore
 }) {
     return (
-        <Router>
+        <BrowserRouter>
             <div className="routes-wrapper">
                 <ToastComponent key="toast" position={'top-right'} theme={store.theme} />
                 <AlertComponent key="alert" theme={store.theme} />
                 <Switch>
-                    <Route path={`/login`} exact component={Login} />
-                    <Route path={`/register`} exact component={Register} />
-                    
-                    {store.user.isAuthenticated ? (
-                        <Route path={''} component={AdminRoute} />
-                    ) : (
-                        <Route path="*" exact component={Login} />
-                    )}
-
-                    <Route component={NotFound} />
+                    <Route path={`/login`}  component={Login} />
+                    <Route path={''} component={AdminRoute} />
                 </Switch>
             </div>
-        </Router>
+        </BrowserRouter>
     );
 });
