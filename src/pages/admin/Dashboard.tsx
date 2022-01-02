79773c33a6ab82866757870385081df443dda853
ice <icebiter@outlook.com>
2022-01-03 03:06:22 +0800

    feat: 增加echarts演示

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -4,7 +4,167 @@ import schema2component from "../../utils/schema2component";
 const schema = {
     type: 'page',
     title: 'Dashboard',
-    body: 'body...'
+    data: {
+        items: [
+            {
+                "projectName": "项目源码",
+                "projectUrl": "https://github.com/iceqing/react-amis-admin",
+            },
+            {
+                "projectName": "项目演示",
+                "projectUrl": "http://amis.iceq.cc/",
+            }
+        ]
+    },
+    body: [
+        {
+            "type": "grid",
+            "className": "m-t-lg",
+            "columns": [
+                {
+                    "type": "card",
+                    "name": "browser",
+                    "header": {
+                        "title": "源码",
+                    },
+                    href: "https://github.com/iceqing/react-amis-admin",
+                    "body": "项目源码：https://github.com/iceqing/react-amis-admin"
+                },
+                {
+                    "type": "card",
+                    "name": "browser",
+                    href: "http://amis.iceq.cc/",
+                    "header": {
+                        "title": "演示",
+                    },
+                    "body": "项目演示地址: http://amis.iceq.cc/"
+                }
+            ]
+        },
+        {
+            "type": "grid",
+            "className": "m-t-lg",
+            "columns": [
+                {
+                    "type": "chart",
+                    "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chart?name=$name&starttime=${starttime}&endtime=${endtime}"
+                },
+                {
+                    "type": "chart",
+                    "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chart2?name=$name"
+                },
+            ]
+        },
+        {
+            "type": "grid",
+            "className": "m-t-lg",
+            "columns": [
+                {
+                    "type": "chart",
+                    "config": {
+                        "legend": {
+                            "formatter": "function (name) { return 'Legend ' + name;}"
+                        },
+                        "dataset": {
+                            "source": [
+                                [
+                                    "type",
+                                    "2012",
+                                    "2013",
+                                    "2014",
+                                    "2015",
+                                    "2016"
+                                ],
+                                [
+                                    "Forest",
+                                    320,
+                                    332,
+                                    301,
+                                    334,
+                                    390
+                                ],
+                                [
+                                    "Steppe",
+                                    220,
+                                    182,
+                                    191,
+                                    234,
+                                    290
+                                ],
+                                [
+                                    "Desert",
+                                    150,
+                                    232,
+                                    201,
+                                    154,
+                                    190
+                                ],
+                                [
+                                    "Wetland",
+                                    98,
+                                    77,
+                                    101,
+                                    99,
+                                    40
+                                ]
+                            ]
+                        },
+                        "xAxis": {
+                            "type": "category",
+                            "axisTick": {
+                                "show": false
+                            }
+                        },
+                        "yAxis": {},
+                        "series": [
+                            {
+                                "type": "bar",
+                                "seriesLayoutBy": "row"
+                            },
+                            {
+                                "type": "bar",
+                                "seriesLayoutBy": "row"
+                            },
+                            {
+                                "type": "bar",
+                                "seriesLayoutBy": "row"
+                            },
+                            {
+                                "type": "bar",
+                                "seriesLayoutBy": "row"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "type": "chart",
+                    "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chartData",
+                    "config": {
+                        "xAxis": {
+                            "type": "category",
+                            "data": [
+                                "Mon",
+                                "Tue",
+                                "Wed",
+                                "Thu",
+                                "Fri",
+                                "Sat"
+                            ]
+                        },
+                        "yAxis": {
+                            "type": "value"
+                        },
+                        "series": [
+                            {
+                                "data": "${line}",
+                                "type": "line"
+                            }
+                        ]
+                    }
+                },
+            ]
+        },
+    ]
 };
 
 export default schema2component(schema);
