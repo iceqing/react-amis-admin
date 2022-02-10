8f12d114e4569ae893131a1f356d5f301ad6ce82
ice <icebiter@outlook.com>
2022-02-10 17:06:50 +0800

    refactor: 重构登陆页面

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,88 +1,47 @@
 import * as React from 'react';
-import AMisRenderer from '../components/AMisRenderer';
-import {inject, observer} from 'mobx-react';
-import {IMainStore} from '../stores';
-import {RouteComponentProps, withRouter} from 'react-router';
+import axios from "axios";
+import {
+    toast,
+} from 'amis';
 
-let ContextPath = '';
+export default class LoginRoute extends React.Component<any, any> {
 
-interface LoginProps extends RouteComponentProps<any> {
-    store: IMainStore;
-}
+    state = {
+        inputUsername: 'admin',
+        inputPassword: 'admin'
+    }
 
-const schema = {
-    type: 'form',
-    submitText: '登录',
-    api: 'post:/api/login',
-    wrapWithPanel: false,
-    messages: {
-        saveSuccess: '登录成功，欢迎光临！'
-    },
-    body: [
-        {
-            children: (props: any) => (
-                <div className="list-group list-group-sm">
-                    {props.renderFormItems({
-                        body: [
-                            {
-                                name: 'username',
-                                children: (props: any) => (
-                                    <div className="list-group-item">
-                                        <input
-                                            placeholder="用户名"
-                                            type="text"
-                                            className="form-control no-shadow no-border"
-                                            value={props.value}
-                                            onChange={(e) => props.onChange(e.currentTarget.value)}
-                                        />
-                                    </div>
-                                )
-                            },
-                            {
-                                name: 'password',
-                                children: (props: any) => (
-                                    <div className="list-group-item">
-                                        <input
-                                            placeholder="密码"
-                                            type="password"
-                                            className="form-control no-shadow no-border"
-                                            onChange={(e) => props.onChange(e.currentTarget.value)}
-                                        />
-                                    </div>
-                                )
-                            }
-                        ]
-                    })}
-                </div>
-            )
-        },
-        {
-            type: 'submit',
-            label: '登录',
-            size: 'lg',
-            inputClassName: 'block w-full',
-            level: 'primary'
-        }
-    ]
-};
-
-@inject("store")
-// @ts-ignore
-@withRouter
-@observer
-export default class LoginRoute extends React.Component<LoginProps, any> {
     handleFormSaved = (value: any) => {
-        const store = this.props.store;
         const history = this.props.history;
-        console.log("### value is ", value)
-        if (value.username != null) {
-            store.user.login(value.username);
-        } else {
-            console.log("usename is null");
-            store.user.login('admin');
-        }
-        console.log("replace history to dashboard, value:", value)
-        history.replace(`/dashboard`)
+        console.log("inputUsername:", this.state.inputUsername)
+        console.log("inputPassword:", this.state.inputPassword)
+        // 这里可以进行登陆密码验证
+        axios.request({
+            method: "post",
+            url: "/api/form/save"
+        }).then(res => {
+            console.log("login res", res);
+            if (res.data != null && res.data.status === 0) {
+                toast['info']('登陆成功', '消息')
+                // 跳转到dashboard页面
+                console.log("replace history to dashboard, value:", value)
+                history.replace(`/dashboard`)
+            } else {
+                toast['error']('登陆失败', '消息')
+            }
+        })
+    }
+
+    handleChangeForPassword = (e: any) => {
+        this.setState({
+            inputPassword: e.target.value
+        })
+    }
+
+    handleChangeForUsername = (e: any) => {
+        this.setState({
+            inputUsername: e.target.value
+        })
     }
 
     render() {
@@ -90,10 +49,29 @@ export default class LoginRoute extends React.Component<LoginProps, any> {
             <div className="app app-header-fixed ">
                 <div className="container w-xxl w-auto-xs">
                     <a className="block m-t-xxl m-b-xl text-center text-2x">XXX 系统登录</a>
-                    <AMisRenderer
-                        onFinished={this.handleFormSaved}
-                        schema={schema}
-                    />
+                    <div className="list-group list-group-sm">
+                        <div className="list-group-item">
+                            <input
+                                placeholder="用户名"
+                                type="text"
+                                className="form-control no-shadow no-border"
+                                onChange={this.handleChangeForUsername}
+                                defaultValue={this.state.inputUsername}
+                            />
+                        </div>
+
+                        <div className="list-group-item">
+                            <input
+                                placeholder="密码"
+                                type="password"
+                                className="form-control no-shadow no-border"
+                                onChange={this.handleChangeForPassword}
+                                defaultValue={this.state.inputPassword}
+                            />
+                        </div>
+                    </div>
+
+                    <button type="button" className="btn btn-info" onClick={this.handleFormSaved}>登陆</button>
                 </div>
             </div>
         );
