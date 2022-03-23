f33ad21407e0964f394963d9647f182997cd98e5
ice <icebiter@outlook.com>
2022-03-24 04:30:17 +0800

    fix: 取消代码注释

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -11,11 +11,11 @@ module.exports = smp.wrap({
   mode: "development",
   entry: {
     app: "./src/index.tsx",
-    // "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
-    // "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
-    // "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
-    // "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
-    // "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker"
+    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+    "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+    "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+    "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+    "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker"
   },
   module: {
     rules: [
