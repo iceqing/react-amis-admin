d5221477fe8047aeda67646fe07aadca5f42846a
ice <icebiter@outlook.com>
2022-03-26 16:10:17 +0800

    feat: 修复会员列表展示问题&添加更多示例

--- mock/index.js
+++ mock/index.js
@@ -1,75 +1,110 @@
 module.exports = {
-    '/api/login': {
+    "/api/login": {
         status: 0,
-        msg: '登录成功'
+        msg: "登录成功",
     },
-    '/api/form/save': {
+    "/api/form/save": {
         status: 0,
-        msg: ''
+        msg: "",
     },
-    '/api/menus': {
+    "/api/menus": {
         status: 0,
         data: [
             {
-                label: '导航',
+                label: "导航",
                 children: [
                     {
-                        path: '/dashboard',
-                        label: 'Dashboard',
-                        icon: 'glyphicon glyphicon-signal',
+                        path: "/dashboard",
+                        label: "Dashboard",
+                        icon: "glyphicon glyphicon-signal",
                     },
                     {
-                        path: '/dialog/simple',
-                        label: 'Dialog',
-                        icon: 'glyphicon glyphicon-signal',
+                        path: "/dialog/simple",
+                        label: "Dialog",
+                        icon: "fa fa-bomb",
                     },
                     {
-                        path: '/icon/simple',
-                        label: 'Icon',
-                        icon: 'glyphicon glyphicon-signal',
-                    },
-                    {
-                        label: '表单页面',
-                        icon: 'glyphicon glyphicon-edit',
+                        label: "会员管理",
+                        icon: "fa fa-user",
                         children: [
                             {
-                                label: '常规表单',
-                                path: '/form/basic',
+                                label: "列表",
+                                path: "/customer/index",
+                            },
+                        ],
+                    },
+                    {
+                        path: "/icon/simple",
+                        label: "Icon",
+
+                        icon: "fa fa-th",
+                    },
+                    {
+                        label: "表单页面",
+                        icon: "glyphicon glyphicon-edit",
+                        children: [
+                            {
+                                label: "常规表单",
+                                path: "/form/basic",
                                 children: [
                                     {
-                                        label: '三级目录测试',
-                                        path: '/form/basic/advanced',
-                                    }
-                                ]
+                                        label: "三级目录测试",
+                                        path: "/form/basic/advanced",
+                                    },
+                                ],
                             },
 
                             {
-                                label: '复杂表单',
-                                path: '/form/advanced',
+                                label: "复杂表单",
+                                path: "/form/advanced",
                             },
 
                             {
-                                label: '向导',
-                                path: '/form/wizard',
+                                label: "向导",
+                                path: "/form/wizard",
                             },
 
                             {
-                                label: '代码编辑器',
-                                path: '/form/editor',
-                            }
-                        ]
-                    },
-                    {
-                        label: '会员管理',
-                        children: [
-                            {
-                                label: '列表',
-                                path: '/customer/index',
-                            }
-                        ]
+                                label: "代码编辑器",
+                                path: "/form/editor",
+                            },
+                        ],
                     }
-                ]
-            }
-        ]
-    }
-}
+                ],
+            },
+        ],
+    },
+    "/api/customer": {
+        status: 0,
+        msg: "",
+        data: {
+            rows: 11,
+            rows: [
+                {
+                    "id": "101",
+                    name: "张三",
+                    age: 20,
+                    gender: 1,
+                    tel: '130****0001',
+                    'company': 'Google'
+                },
+                {
+                    "id": "102",
+                    name: "王丽",
+                    age: 21,
+                    gender: 2,
+                    tel: '131****0002',
+                    'company': 'Baidu'
+                },
+                {
+                    "id": "103",
+                    name: "李四",
+                    age: 21,
+                    gender: 1,
+                    tel: '131****0003',
+                    'company': 'XXX'
+                }
+            ]
+        }
+    },
+};
