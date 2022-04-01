a44bcc94bd15e85e627a774eaa2524e6d462d290
ice <icebiter@outlook.com>
2022-04-01 23:44:13 +0800

    feat: 更新vite版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
 import { viteMockServe } from "vite-plugin-mock";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({
+    plugins: [
+        react({
         babel: {
             plugins: [
                 ["@babel/plugin-proposal-decorators", {legacy: true}],
@@ -24,8 +24,14 @@ export default defineConfig({
         extensions: ['.js', '.ts', '.tsx', '.json'],
         alias: [{find: '@', replacement: '/src/'}],
     },
+    optimizeDeps:{
+        esbuildOptions:{
+            plugins:[
+            ]
+        }
+    },
     server: {
-        port: 6666,
+        port: 4000,
         host: 'localhost',
         open: '/',
     }
