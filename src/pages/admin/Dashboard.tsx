259db3b89dad314d96f47f75cea51a8063eb0e52
ice <icebiter@outlook.com>
2022-07-04 02:02:27 +0800

    fix: 修复代码中的报错

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -81,86 +81,128 @@ const schema = {
                 {
                     "type": "chart",
                     "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chart",
-                },
-                {
-                    "type": "chart",
-                    "config": {
-                        "legend": {
-                            "formatter": "function (name) { return 'Legend ' + name;}"
-                        },
-                        "dataset": {
-                            "source": [
-                                [
-                                    "type",
-                                    "2012",
-                                    "2013",
-                                    "2014",
-                                    "2015",
-                                    "2016"
-                                ],
-                                [
-                                    "Forest",
-                                    320,
-                                    332,
-                                    301,
-                                    334,
-                                    390
-                                ],
-                                [
-                                    "Steppe",
-                                    220,
-                                    182,
-                                    191,
-                                    234,
-                                    290
-                                ],
-                                [
-                                    "Desert",
-                                    150,
-                                    232,
-                                    201,
-                                    154,
-                                    190
-                                ],
-                                [
-                                    "Wetland",
-                                    98,
-                                    77,
-                                    101,
-                                    99,
-                                    40
-                                ]
-                            ]
-                        },
-                        "xAxis": {
-                            "type": "category",
-                            "axisTick": {
-                                "show": false
-                            }
-                        },
-                        "yAxis": {},
-                        "series": [
-                            {
-                                "type": "bar",
-                                "seriesLayoutBy": "row"
-                            },
-                            {
-                                "type": "bar",
-                                "seriesLayoutBy": "row"
-                            },
-                            {
-                                "type": "bar",
-                                "seriesLayoutBy": "row"
-                            },
-                            {
-                                "type": "bar",
-                                "seriesLayoutBy": "row"
-                            }
-                        ]
-                    }
-                },
+                    "interval": 5000
+                }
             ]
-        },
+        }
+        //             "type": "chart",
+        //             "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chartData",
+        //             "config": {
+        //                 "xAxis": {
+        //                     "type": "category",
+        //                     "data": [
+        //                         "Mon",
+        //                         "Tue",
+        //                         "Wed",
+        //                         "Thu",
+        //                         "Fri",
+        //                         "Sat"
+        //                     ]
+        //                 },
+        //                 "yAxis": {
+        //                     "type": "value"
+        //                 },
+        //                 "series": [
+        //                     {
+        //                         "data": "${line}",
+        //                         "type": "line"
+        //                     }
+        //                 ]
+        //             }
+        //         },
+        //         {
+        //             "type": "chart",
+        //             "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chart2?name=$name"
+        //         },
+        //     ]
+        // },
+        // {
+        //     "type": "grid",
+        //     "className": "m-t-lg",
+        //     "columns": [
+        //         {
+        //             "type": "chart",
+        //             "api": "https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/chart/chart",
+        //         },
+        //         {
+        //             "type": "chart",
+        //             "config": {
+        //                 "legend": {
+        //                     "formatter": "function (name) { return 'Legend ' + name;}"
+        //                 },
+        //                 "dataset": {
+        //                     "source": [
+        //                         [
+        //                             "type",
+        //                             "2012",
+        //                             "2013",
+        //                             "2014",
+        //                             "2015",
+        //                             "2016"
+        //                         ],
+        //                         [
+        //                             "Forest",
+        //                             320,
+        //                             332,
+        //                             301,
+        //                             334,
+        //                             390
+        //                         ],
+        //                         [
+        //                             "Steppe",
+        //                             220,
+        //                             182,
+        //                             191,
+        //                             234,
+        //                             290
+        //                         ],
+        //                         [
+        //                             "Desert",
+        //                             150,
+        //                             232,
+        //                             201,
+        //                             154,
+        //                             190
+        //                         ],
+        //                         [
+        //                             "Wetland",
+        //                             98,
+        //                             77,
+        //                             101,
+        //                             99,
+        //                             40
+        //                         ]
+        //                     ]
+        //                 },
+        //                 "xAxis": {
+        //                     "type": "category",
+        //                     "axisTick": {
+        //                         "show": false
+        //                     }
+        //                 },
+        //                 "yAxis": {},
+        //                 "series": [
+        //                     {
+        //                         "type": "bar",
+        //                         "seriesLayoutBy": "row"
+        //                     },
+        //                     {
+        //                         "type": "bar",
+        //                         "seriesLayoutBy": "row"
+        //                     },
+        //                     {
+        //                         "type": "bar",
+        //                         "seriesLayoutBy": "row"
+        //                     },
+        //                     {
+        //                         "type": "bar",
+        //                         "seriesLayoutBy": "row"
+        //                     }
+        //                 ]
+        //             }
+        //         },
+        //     ]
+        // },
     ]
 };
 
